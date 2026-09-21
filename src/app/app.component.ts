99890080028c45d70aa7f47849477cfaab84523e
xRMPx <ryanmichaelpetrillo@gmail.com>
2026-09-21 19:12:45 -0400

    Adds Housing Data and Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'Ryan Petrillo';
 }
