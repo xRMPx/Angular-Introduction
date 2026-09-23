cd76b702edc9ce685d734736b4656f907b16cb7a
xRMPx <ryanmichaelpetrillo@gmail.com>
2026-09-22 22:53:00 -0400

    Add basic routing

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,11 +8,16 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <p>
-      details works!
+      details works! {{ housingLocationId }}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
 
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+  }
 }
