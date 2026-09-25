52e46e42b34cfba814a28036cb36300e9341cc34
xRMPx <ryanmichaelpetrillo@gmail.com>
2026-09-25 19:40:48 -0400

    Form controls,  inputs, and event binding

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housing-location';
+import { first, last } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -116,5 +117,9 @@ export class HousingService {
 
   getHousingLocationById(id: Number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string){
+    console.log(firstName, lastName, email);
   }
 }
