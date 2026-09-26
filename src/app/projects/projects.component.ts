c225e12fc71701130a7608a5af620fca8b6132e5
xRMPx <ryanmichaelpetrillo@gmail.com>
2026-09-26 12:01:47 -0400

    Add basic github contributions heatmap svg

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,11 +7,17 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <p>
-      projects works!
+      Ryan's GitHub Contributions Heatmap:
     </p>
+    <div>
+      <img *ngIf="username" [src]="'https://gh-heat.anishroy.com/api/' + username + '/svg'+ githubSvgArgs" alt="GitHub Contributions Heatmap"/>
+    </div>
   `,
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
+  //move these values to a config file for easier management
+  //https://gh-heat.anishroy.com/
+  username: string = 'xRMPx';
+  githubSvgArgs = '?theme=purple&transparent=true&cellSize=15&cellGap=4&borderWidth=2&showMonthLabels=false';
 }
