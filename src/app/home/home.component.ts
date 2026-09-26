38edb394510ad67de98b6513d136a70f16dd6fd3
xRMPx <ryanmichaelpetrillo@gmail.com>
2026-09-26 09:13:11 -0400

    Add basic search with template variable and filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,12 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" palceholder="Placeholder..."/>
-        <button class="primary" type="button">Search</button>
+        <input type="text" palceholder="Filter by city" #filter/>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation" *ngFor="let housingLocation of housingLocationList">
+      <app-housing-location [housingLocation]="housingLocation" *ngFor="let housingLocation of filteredLocationList">
         </app-housing-location>
     </section>
   `,
@@ -25,11 +25,21 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = [];
+  
   constructor(){
     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
+      this.filteredLocationList = this.housingLocationList;
     });
+
+  }
+  filterResults(text: string){
+    if(!text) this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
 
